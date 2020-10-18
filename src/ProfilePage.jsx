a5b4f9d9e85105ac1994e91ca38f78c90cf352a3
Jonathan Ke <jonathanke@MacBook-Pro.home>
2020-10-17 18:37:48 -0700

    profile page

---
+++ src/ProfilePage.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from "styled-components";
+
+
+
+class ProfilePage extends React.Component {
+
+    constructor(props){
+        let URLParse = new URLSearchParams(this.props.location.search);
+        this.state = {
+            username : URLParse.get("username"),
+            isLoaded: false,
+            error: null
+        };
+    }
+
+    componentDidMount() {
+        fetch('http://34.72.70.68:8080/infoUser'+'?user='+this.state.username, 
+          {'mode':'no-cors'})
+            .then(res => res.json())
+            .then((result) => 
+                {
+                    console.log(result);
+                    this.setState({
+                        isLoaded : true,
+                        username : this.state.username,
+                        info : result,
+                    });
+                },
+                // Note: it's important to handle errors here
+                 // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                      this.setState({
+                      isLoaded: true,
+                      error: error
+                    });
+                });
+    }
+
+    render() {
+        const error = this.state.error ;
+        const isLoaded = this.state.isLoaded;
+        if (error) {
+          return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+          return <div>Loading...</div>;
+        } else {
+          const info = this.state.info
+          return (
+            <center>
+              
+            </center>  
+          );
+        }
+    }
+}
