bc93cd5c3fa64f1bfe63e90b63ec3fffbc760a25
Jonathan Ke <jonathanke@MacBook-Pro.home>
2020-10-17 20:35:27 -0700

    registration page

--- src/App.js
+++ src/App.js
@@ -37,8 +37,7 @@ export default class App extends Component {
     return (
       <Router history={history}>
         <Switch>
-          <Route path="/register" >
-            <RegistrationPage />
+          <Route path="/register" RegistrationPage />
           </Route>
           <Route path="/">
             <HomePage />
