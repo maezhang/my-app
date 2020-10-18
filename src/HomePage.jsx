47bdb39091d3f9e65f1935b431384dc5265fd540
Tinghua Hsu <tinghuahsu77@gmail.com>
2020-10-18 01:50:43 -0400

    del password

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -65,21 +65,7 @@ function HomePage() {
                   <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
 
       </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
+      
       <Form.Item>
         <Button type="primary" htmlType="submit" className="login-form-button">
           Log in
